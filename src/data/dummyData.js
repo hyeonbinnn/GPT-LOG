39dcc2f1543132adc68737a5ac4b2ca98a0eb1ef
hyeonbin jeong <duafuuu@gmail.com>
2024-12-26 01:17:00 +0900

    add: 사용자 입력값 데이터 초기 더미 데이터

---
+++ src/data/dummyData.js
@@ -0,0 +1,11 @@
+import defaultImage from '../assets/defaultImage.png';
+
+export const dummyData = {
+  title: '일기장',
+  thumbnail: defaultImage,
+  summary: '오늘의 일기를 한 줄로 요약해줄게!',
+  emotional_content: '오늘의 일기를 만들어줄게!',
+  emotional_result: '오늘의 일기를 통해 내가 느낀 감정들을 말해줄게!',
+  analysis: '오늘의 일기를 바탕으로 심리 분석을 해줄게!',
+  action_list: ['GPT 조언 1', 'GPT 조언 2', 'GPT 조언 3'],
+};
